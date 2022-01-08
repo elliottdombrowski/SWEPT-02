0d4d68b1e8e3c25fb78e4273dbf1cdf1a1e1bbc4
elliott dombrowski <elliottroyal@me.com>
2022-01-08 15:11:27 -0600

    created sweeper page and edited App.js to render by default

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,14 @@ import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Navbar from '../src/components/Navbar/Navbar';
+import Sweeper from '../src/pages/Sweeper/Sweeper';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navbar />
+        <Sweeper />
       </Router>
     </div>
   );
---
+++ client/src/pages/Sweeper/Sweeper.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import './sweeper.css';
+
+const Sweeper = () => {
+  return (
+    <div>hi</div>
+  );
+};
+
+export default Sweeper;
