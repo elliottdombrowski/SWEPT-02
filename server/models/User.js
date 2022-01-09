532d09262732370cb9d4075ab80961e8e49705d1
Sadie Sial <sadiesial@gmail.com>
2022-01-09 15:39:19 -0600

    name portion added

--- server/models/User.js
+++ server/models/User.js
@@ -2,7 +2,16 @@ const mongoose = require('mongoose');
 
 // userSchema:
 const userSchema = new Schema({
-
+    firstName: {
+        type: String,
+        required: true,
+        trim: true
+      },
+      lastName: {
+        type: String,
+        required: true,
+        trim: true
+      },
   });
 
 module.exports = User;
