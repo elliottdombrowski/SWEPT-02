09d486e98c17ebba327dadc25b8e99cf5d22baff
Sadie Sial <sadiesial@gmail.com>
2022-01-09 15:39:48 -0600

    added email + pass to schema

--- server/models/User.js
+++ server/models/User.js
@@ -6,12 +6,22 @@ const userSchema = new Schema({
         type: String,
         required: true,
         trim: true
-      },
-      lastName: {
+    },
+    lastName: {
         type: String,
         required: true,
         trim: true
-      },
-  });
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
+});
 
 module.exports = User;
