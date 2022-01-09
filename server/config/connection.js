4f7b7bd9747074d91e78477ba493545e9ba868bf
Sadie Sial <sadiesial@gmail.com>
2022-01-09 15:20:51 -0600

    uri + swept db added

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,1 +1,8 @@
 const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/swept', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
