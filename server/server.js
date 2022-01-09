0b52d3f2b2dc3b113a238f9e647a9c5d612897c5
elliott dombrowski <elliottroyal@me.com>
2022-01-09 13:21:27 -0600

    configured server-side jwt auth.js

---
+++ server/server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
