38efefcf25078faefbe1151a1dcfe3be1a0e6fc0
elliott dombrowski <elliottroyal@me.com>
2022-01-09 13:01:46 -0600

    added login component jsx

--- client/src/components/LoginForm/LoginForm.js
+++ client/src/components/LoginForm/LoginForm.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
+import './loginform.css';
 
 import Auth from '../../utils/auth';
 
 const LoginForm = () => {
   const [loginData, setLoginData] = useState({ email: '', password: '' });
   const [login, { error, data }] = useMutation(LOGIN_USER);
-  const [validated] = useState(false);
+  // const [validated] = useState(false);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -17,11 +18,11 @@ const LoginForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
+    // const form = event.currentTarget;
+    // if (form.checkValidity() === false) {
+    //   event.preventDefault();
+    //   event.stopPropagation();
+    // }
 
     try {
       const { data } = await login({
@@ -32,10 +33,47 @@ const LoginForm = () => {
     } catch (err) {
       console.log(err);
     }
+
+    setLoginData({
+      username: '',
+      email: '',
+      password: '',
+    })
   };
 
   return (
-    login
+    <div className='login-wrapper'>
+      <form className='login-form' onSubmit={handleFormSubmit}>
+        <input
+          type='text'
+          name='email'
+          onChange={handleInputChange}
+          value={loginData.email}
+          required
+          placeholder='Your Email'
+          className='login-input'
+        />
+        
+        <input
+          type='text'
+          name='password'
+          onChange={handleInputChange}
+          value={loginData.password}
+          required
+          placeholder='Your Password'
+          className='login-input'
+        />
+
+        <button
+          disabled={!(loginData.email && loginData.password)}
+          type='submit'
+          className='login-input login-submit-btn'
+        >
+          Log In
+        </button>
+
+      </form>
+    </div>
   );
 }
 
