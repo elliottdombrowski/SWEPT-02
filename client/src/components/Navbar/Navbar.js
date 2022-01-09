bf3daa379c0777e2d846658c3e0542b60a38a96c
elliott dombrowski <elliottroyal@me.com>
2022-01-08 20:32:05 -0600

    added css hamburger menu and media queries to display it

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import './query.css';
 
 const Navbar = () => {
   return (
@@ -29,6 +30,12 @@ const Navbar = () => {
             <Link to='/login' className='nav-links'>
               SIGN UP
             </Link>
+          </div>
+
+          <div className='hamburger'>
+            <span className='bar'></span>
+            <span className='bar'></span>
+            <span className='bar'></span>
           </div>
         </div>
       </header>
