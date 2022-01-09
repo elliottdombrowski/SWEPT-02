77b6ab9327652e85faab99452031a89a248465c4
elliott dombrowski <elliottroyal@me.com>
2022-01-08 22:32:44 -0600

    connected snow/sweeper buttons in mobile nav bar. gotta refactor to transition nicely. later.

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -5,12 +5,29 @@ import { Link } from 'react-router-dom';
 import './navbar.css';
 import './query.css';
 
-const profile = <FontAwesomeIcon icon={faUserCircle} className='fa-lg'/>
+const profile = <FontAwesomeIcon icon={faUserCircle} className='fa-lg' />
 
 const Navbar = () => {
   const mobileMenu = () => {
     document.getElementById('hamburger').classList.toggle('active');
     document.getElementById('navbar-right').classList.toggle('active');
+  };
+
+  const mobileNavChangePage = () => {
+    document.getElementById('mobile-nav-snow').classList.toggle('active');
+    document.getElementById('mobile-nav-sweeper').classList.toggle('active');
+    const overlay = document.getElementById('nav-overlay');
+
+    if (document.getElementById('mobile-nav-snow').classList.contains('active')) {
+      overlay.style.right = '0';
+      overlay.style.left = '50%';
+      document.getElementById('mobile-nav-sweeper').classList.remove('active');
+    }
+    if (document.getElementById('mobile-nav-sweeper').classList.contains('active')) {
+      overlay.style.left = '0';
+      overlay.style.right = '50%';
+      document.getElementById('mobile-nav-snow').classList.remove('active');
+    }
   };
 
   const mobile = window.matchMedia("(max-width: 768px)");
@@ -30,13 +47,15 @@ const Navbar = () => {
         <div className='nav-right' id='navbar-right'>
           {mobile.matches ? (
             <div className='nav-right-mobile'>
-              <Link to='/snow' className='nav-item nav-links nav-mobile'>
-                SNOW
-              </Link>
-              
-              <Link to='/snow' className='nav-item nav-links nav-mobile'>
+              <Link to='/sweeper' className='nav-item nav-links nav-mobile mobile-page-links mobile-nav-sweeper' id='mobile-nav-sweeper' onClick={() => mobileNavChangePage()}>
                 SWEEPER
               </Link>
+
+              <Link to='/snow' className='nav-item nav-links nav-mobile mobile-page-links mobile-nav-snow' id='mobile-nav-snow' onClick={() => mobileNavChangePage()}>
+                SNOW
+              </Link>
+              <div className='nav-right-mobile-overlay' id='nav-overlay'>
+              </div>
             </div>
           ) : (
             <Link to="/snow" className='nav-item nav-links'>
@@ -50,9 +69,9 @@ const Navbar = () => {
               {profile}
             </Link>
           ) : (
-          <Link to='/me' className='nav-item nav-links'>
-            PROFILE
-          </Link>
+            <Link to='/me' className='nav-item nav-links'>
+              PROFILE
+            </Link>
           )}
 
           <div className='login-btn'>
