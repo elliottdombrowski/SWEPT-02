8253092d76d51667df7af1a067e863f0048174da
Sadie Sial <sadiesial@gmail.com>
2022-01-09 21:24:28 -0600

    street object

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -3,12 +3,15 @@ const { Schema } = mongoose;
 const User = require('./User')
 
 const profileSchema = new Schema({
-username: {
-    type: String,
-    required: true,
-    unique: true
-},
-
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    street: {
+        type: string,
+        trim: true
+    },
 
 });
 
