e800476a0143a02c750b0b33dfe25079543a39a4
Sadie Sial <sadiesial@gmail.com>
2022-01-09 23:15:13 -0600

    dummy account entries for mongo

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -2,7 +2,21 @@ const db = require('../config/connection');
 const { User } = require('../models');
 
 db.once('open', async () => {
+    await User.create({
+        username: 'test',
+        email: 'test@test.com',
+        password: 'test1234',
+    });
 
+    await User.create({
+        username: 'sadie',
+        email: 'sadie@sadie.com',
+        password: 'sadie1234',
+    });
+
+    console.log('dummy users seeded');
+
+    process.exit();
 
 }
 );
