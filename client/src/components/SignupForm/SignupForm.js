d750e5c5d97f4be863e7a175d6cb55ff0f1991af
elliott dombrowski <elliottroyal@me.com>
2022-01-09 18:32:56 -0600

    ported login form to signup component, edited to reflect signup

--- client/src/components/SignupForm/SignupForm.js
+++ client/src/components/SignupForm/SignupForm.js
@@ -1,13 +1,91 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { ADD_USER } from '../../utils/mutations';
+import { LOGIN_USER } from '../../utils/mutations';
+import './loginform.css';
 
 import Auth from '../../utils/auth';
 
-const SignupForm = () => {
+const LoginForm = () => {
+  const [signupData, setSignupData] = useState({ username: '', email: '', password: '' });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+  const [validated] = useState(false);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setSignupData({ ...signupData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // const form = event.currentTarget;
+    // if (form.checkValidity() === false) {
+    //   event.preventDefault();
+    //   event.stopPropagation();
+    // }
+
+    try {
+      const { data } = await login({
+        variables: { ...signupData },
+      });
+
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.log(err);
+    }
+
+    setSignupData({
+      username: '',
+      email: '',
+      password: '',
+    })
+  };
+
   return (
-    signup
+    <div className='login-wrapper'>
+      <div className='login-form-wrapper'>
+        <form className='login-form' onSubmit={handleFormSubmit}>
+          <input
+            type='text'
+            name='username'
+            onChange={handleInputChange}
+            value={signupData.username}
+            required
+            placeholder='Your Name'
+            className='login-input'
+          />
+
+          <input
+            type='text'
+            name='email'
+            onChange={handleInputChange}
+            value={signupData.email}
+            required
+            placeholder='Your Email'
+            className='login-input'
+          />
+          
+          <input
+            type='text'
+            name='password'
+            onChange={handleInputChange}
+            value={signupData.password}
+            required
+            placeholder='Your Password'
+            className='login-input'
+          />
+
+          <button
+            disabled={!(signupData.email && signupData.password)}
+            type='submit'
+            className='login-input login-submit-btn'
+          >
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }
 
-export default SignupForm;
+export default LoginForm;
