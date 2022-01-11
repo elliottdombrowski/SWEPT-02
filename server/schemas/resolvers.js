3f956a592aebbb5759214c64f1dbed1e805a5c54
elliott dombrowski <elliottroyal@me.com>
2022-01-11 15:29:17 -0600

    fixed signup mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,11 +1,21 @@
-const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models')
 const { signToken } = require('../utils/auth');
 
 
 const resolvers = {
     Query: {},
-    Mutation: {}
+    Mutation: {
+      addUser: async (parent, args) => {
+        try {
+          const user = await User.create(args);
+          const token = signToken(user);
+
+          return { token, user };
+        } catch (error) {
+          console.log(error);
+        }
+      }
+    }
 };
 
 
