b9f3fbc20cd9a33cc667316d7b8abaf20e0a0790
Sadie Sial <sadiesial@gmail.com>
2022-01-10 22:59:15 -0600

    using starter ajax testing

--- server/api/sweeper.js
+++ server/api/sweeper.js
@@ -1,2 +1,16 @@
 const axios = require("axios");
 require('dotenv').config({ path: '../../.env' });
+
+const sweeperURL = 'https://data.cityofchicago.org/resource/wvjp-8m67.json';
+
+$.ajax({
+    url: "https://data.cityofchicago.org/resource/wvjp-8m67.json",
+    type: "GET",
+    data: {
+      "$limit" : 5000,
+      "$$app_token" : "wuWBoPJo0VvB887VUDjq8qYJ8"
+    }
+}).done(function(data) {
+  alert("Retrieved " + data.length + " records from the dataset!");
+  console.log(data);
+});
