c4522e7b3223e8a7747899b20876ffd91f4268c5
Justin Kemp <jkemp0790@gmail.com>
2022-01-11 17:41:17 -0600

    more changes to profile page

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -3,18 +3,18 @@ import './profile.css';
 
 const Profile = () => {
   return (
-      <div className='profile-wrapper'>
-          <div className='profile-info-wrapper'>
-              <div className='profile-welcome-header'>
-                <h1>Welcome, Name!</h1>
-              </div>
-          </div>
-          <div className='recent-search-wrapper'>
-              <div className='recent-search-header'>
-                <h1>Recent Searches</h1>
-              </div>
-          </div>
-      </div>
+    <div className='profile-wrapper'>
+        <div className='profile-info-wrapper'>
+            <div className='profile-welcome-header'>
+              <h1>Welcome, Name!</h1>
+            </div>
+        </div>
+        <div className='recent-search-wrapper'>
+            <div className='recent-search-header'>
+              <h1>Recent Searches</h1>
+            </div>
+        </div>
+    </div>
   );
 };
 
