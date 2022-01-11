3cffc1812a5878cf4790629ac9a13f932735a73d
Sadie Sial <sadiesial@gmail.com>
2022-01-11 00:54:12 -0600

    get method for /sweeper with call

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const router = express.Router();
 const Sweeper = require('./sweeper');
 
-
+router.get('/sweeper', async (req, res) => {
+    let sweeper = new Sweeper()
+    res.header("Content-Type", 'application/json');
+    res.send(JSON.stringify);
+});
 
 module.exports = router;
