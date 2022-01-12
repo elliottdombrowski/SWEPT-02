80fbedccd542613a7cd4ac1cfcfb23f0c8c285a9
Sadie Sial <sadiesial@gmail.com>
2022-01-12 00:21:41 -0600

    ward api call for zip code retrieval

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -5,56 +5,62 @@ const { AuthenticationError } = require('apollo-server-express');
 
 
 const resolvers = {
-    Query: {
-      getWard: async (parent, args, context) => {
-        try {
-          console.log('is this undefined?', args);
-          if (!args.wardNumber) {
-            return null;
-          }
-          const sweeperData = {
-              method: 'GET',
-              url: 'https://data.cityofchicago.org/resource/wvjp-8m67.json',
-              data: {
-                  '$limit': 5,
-                  '$$app_token': process.env.SWEEPER
-              }
-          }
-          const sweeperResponse = await axios.request(sweeperData)
-          // console.log(sweeperResponse.data);
-          return sweeperResponse.data;
-        } catch (error) {
-          console.log(error);
+  Query: {
+    // ward from *sweeper scheule* api
+    getWard: async (parent, args, context) => {
+      try {
+        console.log('is this undefined?', args);
+        if (!args.wardNumber) {
+          return null;
         }
+        const sweeperData = {
+          method: 'GET',
+          url: 'https://data.cityofchicago.org/resource/wvjp-8m67.json',
+          data: {
+            '$limit': 5,
+            '$$app_token': process.env.SWEEPER
+          }
+        }
+        const sweeperResponse = await axios.request(sweeperData)
+        // console.log(sweeperResponse.data);
+        return sweeperResponse.data;
+      } catch (error) {
+        console.log(error);
       }
     },
-    Mutation: {
-      addUser: async (parent, args) => {
-        try {
-          const user = await User.create(args);
-          const token = signToken(user);
+    // zipcode from *general ward* api
+    getZip: async (parents, args, context) => {
 
-          return { token, user };
-        } catch (error) {
-          console.log(error);
-        }
       },
 
-      login: async (parent, { email, password }) => {
-        const user = await User.findOne({ email });
-        if (!user) {
-          throw new AuthenticationError('no user found with this email');
-        }
-
-        const correctPw = await user.isCorrectPassword(password);
-        if (!correctPw) {
-          throw new AuthenticationError('incorrect password.');
-        }
-
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      try {
+        const user = await User.create(args);
         const token = signToken(user);
+
         return { token, user };
+      } catch (error) {
+        console.log(error);
       }
+    },
+
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+      if (!user) {
+        throw new AuthenticationError('no user found with this email');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new AuthenticationError('incorrect password.');
+      }
+
+      const token = signToken(user);
+      return { token, user };
     }
+  }
 };
 
 
