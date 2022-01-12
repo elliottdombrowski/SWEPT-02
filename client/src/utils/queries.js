f8b2e4fc401da735c31c67ceb0ade9a4c9afa463
Sadie Sial <sadiesial@gmail.com>
2022-01-12 00:33:15 -0600

    set up export variable GET_ZIP fo front end bidness

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -12,3 +12,22 @@ export const GET_WARD = gql `
     }
   }
 `;
+
+export const GET_ZIP = gql `
+  query getZip($zipNumber: String) {
+    getZip(zipNumber: $zipNumber) {
+      ward: String,
+      alderman: String,
+      address: String,
+      city: String,
+      state: String,
+      zipcode: String,
+      ward_phone: String,
+      ward_fax: String,
+      email: String,
+      website: {
+        url: String
+      },
+    }
+  }
+`;
