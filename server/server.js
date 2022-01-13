afb317fccb2c164c3b668d315815003b9995d53e
elliott dombrowski <elliottroyal@me.com>
2022-01-13 17:56:32 -0600

    removed starter stripe stuff in server

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const db = require('./config/connection');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const stripe = require('stripe')(process.env.STRIPE_SECRET_TEST);
 require('dotenv').config();
 
 const { ApolloServer } = require('apollo-server-express');
@@ -23,36 +22,6 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
-
-app.post('/payment', cors(), async (req, res) => {
-  let { amount, id } = req.body;
-  try {
-    const payment = await stripe.paymentIntents.create({
-      amount,
-      currency: "USD",
-      description: "Developer donation",
-      payment_method: id,
-      confirm: true
-    });
-    console.log('payment- ', payment)
-    res.json({
-      message: "Payment successful",
-      success: true
-    });
-  } catch (error) {
-    console.log("error ", error)
-    res.json({
-      message: "Payment not successful",
-      success: false
-    })
-  }
-});
-
-//FOR STRIPE PAYMENT
-
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
