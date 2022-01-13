09654b48b0ee87c7d6cdf0e9f58864c00b4a08d0
elliott dombrowski <elliottroyal@me.com>
2022-01-13 15:16:07 -0600

    edited keys

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const stripe = require('stripe')(process.env.STRIPE_SECRET);
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 require('dotenv').config();
 
 const { ApolloServer } = require('apollo-server-express');
@@ -23,33 +23,33 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
-// app.post('/payment', cors(), async (req, res) => {
-//   let { amount, id } = req.body;
-//   try {
-//     const payment = await stripe.paymentIntents.create({
-//       amount,
-//       currency: "USD",
-//       description: "Developer donation",
-//       payment_method: id,
-//       confirm: true
-//     });
-//     console.log('payment- ', payment)
-//     res.json({
-//       message: "Payment successful",
-//       success: true
-//     });
-//   } catch (error) {
-//     console.log("error ", error)
-//     res.json({
-//       message: "Payment not successful",
-//       success: false
-//     })
-//   }
-// });
+app.post('/payment', cors(), async (req, res) => {
+  let { amount, id } = req.body;
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "Developer donation",
+      payment_method: id,
+      confirm: true
+    });
+    console.log('payment- ', payment)
+    res.json({
+      message: "Payment successful",
+      success: true
+    });
+  } catch (error) {
+    console.log("error ", error)
+    res.json({
+      message: "Payment not successful",
+      success: false
+    })
+  }
+});
 
 //FOR STRIPE PAYMENT
 
