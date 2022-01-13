bbbb9821afdf4ff5146dbc2092537f3769ce8c45
elliott dombrowski <elliottroyal@me.com>
2022-01-12 22:37:27 -0600

    testing query port

--- client/src/components/FindZipForm/FindZipForm.js
+++ client/src/components/FindZipForm/FindZipForm.js
@@ -1,125 +1,125 @@
-import React, { useState, useRef } from 'react';
-import './findzipform.css';
-
-import { GET_ZIP } from '../../utils/queries';
-import { useQuery } from '@apollo/client';
-
-const dataContext = React.createContext()
-
-const FindZipForm = () => {
-  const zipNumber = useRef('');
-  const [zips, setZips] = useState('');
-  const { loading, data } = useQuery(GET_ZIP, {
-    variables: { zipNumber: zips }
-  });
-
-  const zipInfo = data?.getZip || [];
-  console.log(zipInfo);
-
-  const zipNumberSubmit = async (event) => {
-    event.preventDefault();
-    console.log(zipNumber.current.value);
-    setZips(zipNumber.current.value)
-    return true;
-  };
-
-  return (
-    <>
-      <form
-        onSubmit={(event) => zipNumberSubmit(event)}
-        className='zipform-wrapper'
-      >
-        <input
-          // value={}
-          // onChange={}
-          ref={zipNumber}
-          name='zipNumber'
-          placeholder='Enter your Zip Code!'
-          className='zipform-input'
-        />
-
-        <button
-          type='submit'
-          className='zipform-input zipform-btn'
-        >
-          Find your schedule!
-        </button>
-      </form>
-      <div>
-        {
-          zipInfo.map((info, index) => {
-            return (
-              <div className='sweeper-data-output-wrapper' key={index}>
-                <h4>{info.zipcode} belongs to ward:</h4>
-                <h3>{info.ward}</h3>
-              </div>
-            )
-          })
-        }
-      </div>
-    </>
-  );
-};
-
-export default FindZipForm;
-
-
-
-
-
-
-
-
-
-// sadie did this thing and it's crapola
-// ripped copy and edit from FindWardForm btw
-
-// import React, { useRef, useState } from 'react';
+// import React, { useState, useRef } from 'react';
 // import './findzipform.css';
 
 // import { GET_ZIP } from '../../utils/queries';
 // import { useQuery } from '@apollo/client';
 
+// const dataContext = React.createContext()
+
 // const FindZipForm = () => {
 //   const zipNumber = useRef('');
-//   const [zip, setZip] = useState('');
+//   const [zips, setZips] = useState('');
 //   const { loading, data } = useQuery(GET_ZIP, {
-//     variables: { zipNumber: zip }
+//     variables: { zipNumber: zips }
 //   });
+
 //   const zipInfo = data?.getZip || [];
 //   console.log(zipInfo);
 
 //   const zipNumberSubmit = async (event) => {
 //     event.preventDefault();
-
 //     console.log(zipNumber.current.value);
-//     setZip(zipNumber.current.value);
-
+//     setZips(zipNumber.current.value)
 //     return true;
 //   };
 
 //   return (
-//     <form
-//       onSubmit={(event) => zipNumberSubmit(event)}
-//       className='zipform-wrapper'
-//     >
-//       <input
-//         // value={}
-//         // onChange={}
-//         ref={zipNumber}
-//         name='zipNumber'
-//         placeholder='Enter your Zip Code!'
-//         className='zipform-input'
-//       />
-
-//       <button
-//         type='submit'
-//         className='zipform-input zipform-btn'
+//     <>
+//       <form
+//         onSubmit={(event) => zipNumberSubmit(event)}
+//         className='zipform-wrapper'
 //       >
-//         Find your schedule!
-//       </button>
-//     </form>
+//         <input
+//           // value={}
+//           // onChange={}
+//           ref={zipNumber}
+//           name='zipNumber'
+//           placeholder='Enter your Zip Code!'
+//           className='zipform-input'
+//         />
+
+//         <button
+//           type='submit'
+//           className='zipform-input zipform-btn'
+//         >
+//           Find your schedule!
+//         </button>
+//       </form>
+//       <div>
+//         {
+//           zipInfo.map((info, index) => {
+//             return (
+//               <div className='sweeper-data-output-wrapper' key={index}>
+//                 <h4>{info.zipcode} belongs to ward:</h4>
+//                 <h3>{info.ward}</h3>
+//               </div>
+//             )
+//           })
+//         }
+//       </div>
+//     </>
 //   );
 // };
 
 // export default FindZipForm;
+
+
+
+
+
+
+
+
+
+// // sadie did this thing and it's crapola
+// // ripped copy and edit from FindWardForm btw
+
+// // import React, { useRef, useState } from 'react';
+// // import './findzipform.css';
+
+// // import { GET_ZIP } from '../../utils/queries';
+// // import { useQuery } from '@apollo/client';
+
+// // const FindZipForm = () => {
+// //   const zipNumber = useRef('');
+// //   const [zip, setZip] = useState('');
+// //   const { loading, data } = useQuery(GET_ZIP, {
+// //     variables: { zipNumber: zip }
+// //   });
+// //   const zipInfo = data?.getZip || [];
+// //   console.log(zipInfo);
+
+// //   const zipNumberSubmit = async (event) => {
+// //     event.preventDefault();
+
+// //     console.log(zipNumber.current.value);
+// //     setZip(zipNumber.current.value);
+
+// //     return true;
+// //   };
+
+// //   return (
+// //     <form
+// //       onSubmit={(event) => zipNumberSubmit(event)}
+// //       className='zipform-wrapper'
+// //     >
+// //       <input
+// //         // value={}
+// //         // onChange={}
+// //         ref={zipNumber}
+// //         name='zipNumber'
+// //         placeholder='Enter your Zip Code!'
+// //         className='zipform-input'
+// //       />
+
+// //       <button
+// //         type='submit'
+// //         className='zipform-input zipform-btn'
+// //       >
+// //         Find your schedule!
+// //       </button>
+// //     </form>
+// //   );
+// // };
+
+// // export default FindZipForm;
--- client/src/pages/Sweeper/Sweeper.js
+++ client/src/pages/Sweeper/Sweeper.js
@@ -1,11 +1,10 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowUp } from '@fortawesome/free-solid-svg-icons';
-import FindWardForm from '../../components/FindWardForm/FindWardForm';
 import { GET_ZIP } from '../../utils/queries';
+import { GET_WARD } from '../../utils/queries';
 import { useQuery } from '@apollo/client';
 import './sweeper.css';
-import './findzipform.css';
 
 const arrow = <FontAwesomeIcon icon={faArrowUp} />
 
@@ -13,19 +12,30 @@ const dataContext = React.createContext()
 
 const Sweeper = () => {
   const zipNumber = useRef('');
+  const wardNumber = useRef('');
   const [zips, setZips] = useState('');
+  const [ward, setWard] = useState('');
 
   //SETS ZIP VS WARD FORM
   const [setForm, setFindForm] = useState(false);
   //SETS LINK MESSAGE
   const changeForm = setForm ? 'zip code' : 'ward';
+
+  //ZIPCODE FORM USEQUERY
   const { loading, data } = useQuery(GET_ZIP, {
     variables: { zipNumber: zips }
   });
-
   const zipInfo = data?.getZip || [];
   console.log(zipInfo);
 
+  //WARD FORM USEQUERY
+  const { load, dataLoad } = useQuery(GET_WARD, {
+    variables: { wardNumber: ward }
+  });
+  const wardInfo = dataLoad?.getWard || [];
+  console.log(wardInfo);
+
+  //ZIP FORM SUBMIT
   const zipNumberSubmit = async (event) => {
     event.preventDefault();
     console.log(zipNumber.current.value);
@@ -33,46 +43,59 @@ const Sweeper = () => {
     return true;
   };
 
+  //WARD FORM SUBMIT
+  const wardNumberSubmit = async (event) => {
+    event.preventDefault();
+    console.log(wardNumber.current.value);
+    setWard(wardNumber.current.value);
+    return true;
+  };
+
   return (
     <div className='sweeper-wrapper'>
       <div className='sweeper-form-wrapper'>
         <div className='zip-form-wrapper'>
-          <form
-            onSubmit={(event) => zipNumberSubmit(event)}
-            className='zipform-wrapper'
-          >
-            {setForm ? (
-              <div className='form-wrapper'>
-                <input
-                  // value={}
-                  // onChange={}
-                  ref={zipNumber}
-                  name='zipNumber'
-                  placeholder='Enter your Zip Code!'
-                  className='zipform-input'
-                />
-              </div>
-            ) : (
-              <div className='form-wrapper'>
-                <input
-                  // value={}
-                  // onChange={}
-                  ref={zipNumber}
-                  name='zipNumber'
-                  placeholder='Enter your Ward Number!'
-                  className='zipform-input'
-                />
-              </div>
-            )}
-
-
-            <button
-              type='submit'
-              className='zipform-input zipform-btn'
+          {setForm ? (
+            <form
+              onSubmit={(event) => zipNumberSubmit(event)}
+              className='zipform-wrapper'
             >
-              Find your schedule!
-            </button>
-          </form>
+              <input
+                // value={}
+                // onChange={}
+                ref={zipNumber}
+                name='zipNumber'
+                placeholder='Enter your Zip Code!'
+                className='zipform-input'
+              />
+              <button
+                type='submit'
+                className='zipform-input zipform-btn'
+              >
+                Find your schedule!
+              </button>
+            </form>
+          ) : (
+            <form
+              onSubmit={(event) => wardNumberSubmit(event)}
+              className='zipform-wrapper'
+            >
+              <input
+                // value={}
+                // onChange={}
+                ref={wardNumber}
+                name='wardNumber'
+                placeholder='Enter your Ward Number!'
+                className='zipform-input'
+              />
+              <button
+                type='submit'
+                className='zipform-input zipform-btn'
+              >
+                Find your schedule!
+              </button>
+            </form>
+          )}
         </div>
 
         <a
@@ -80,25 +103,42 @@ const Sweeper = () => {
           rel="noopener noreferrer"
           className='find-ward'
           onClick={() => setFindForm(prev => !prev)}
-          >
+        >
           Don't know your {changeForm}?
           <span className='sweeper-arrow'>{arrow}</span>
         </a>
       </div>
 
       {/* RENDER DATA HERE */}
-          <div className='sweeper-data-output-wrapper'>
-            {
-              zipInfo.map((info, index) => {
-                return (
-                  <div className='sweeper-data-output' key={index}>
-                    <h4>{info.zipcode} belongs to ward:</h4>
-                    <h3>{info.ward}</h3>
-                  </div>
-                )
-              })
-            }
-          </div>
+      {setForm ? (
+        <div className='sweeper-data-output-wrapper'>
+          {
+            zipInfo.map((info, index) => {
+              return (
+                <div className='sweeper-data-output' key={index}>
+                  <h4>{info.zipcode} belongs to ward:</h4>
+                  <h3>{info.ward}</h3>
+                </div>
+              )
+            })
+          }
+        </div>
+      ) : (
+        <div className='sweeper-data-output-wrapper'>
+          {
+            wardInfo.map((info, index) => {
+              return (
+                <div className='sweeper-data-output-wrapper' key={index}>
+                  <h3>Dates: {info.dates}</h3>
+                  <h4>Month_Date: {info.month_name}</h4>
+                  <h2>Ward: {info.ward}</h2>
+                </div>
+              )
+            })
+          }
+        </div>
+      )}
+
     </div>
   );
 };
