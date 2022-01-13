5e9feb6b05fe4d0cc46ab59a2224425b49a7e32a
Sadie Sial <sadiesial@gmail.com>
2022-01-12 20:49:46 -0600

    snow info spit out

--- client/src/components/FindStreetForm/FindStreetForm.js
+++ client/src/components/FindStreetForm/FindStreetForm.js
@@ -17,34 +17,47 @@ const FindStreetForm = () => {
 
   const snowNumberSubmit = async (event) => {
     event.preventDefault();
-    console.log(wardNumber.current.value);
+    console.log(snowNumber.current.value);
     setSnow(snowNumber.current.value);
     return true;
   };
 
   return (
-    <form
-      onSubmit={(event) => snowNumberSubmit(event)}
-      className='snow-street-form'
-    >
-      <input
-        // value={}
-        // onChange={}
-        ref={snowNumber}
-        name='zipNumber'
-        placeholder="Enter your Street Name!"
-        className='street-input'
-      />
-
-      <button
-        type='submit'
-        className='street-input street-btn'
+    <>
+      <form
+        onSubmit={(event) => snowNumberSubmit(event)}
+        className='snow-street-form'
       >
-        Find your schedule!
-      </button>
-    </form>
+        <input
+          // value={}
+          // onChange={}
+          ref={snowNumber}
+          name='snowNumber'
+          placeholder="Enter your Street Name!"
+          className='street-input'
+        />
 
-    
+        <button
+          type='submit'
+          className='street-input street-btn'
+        >
+          Find your schedule!
+        </button>
+      </form>
+      <div>
+        {
+          snowInfo.map((info, index) => {
+            return (
+              <div className='sweeper-data-output-wrapper' key={index}>
+                <h2>On: {info.on_street}</h2>
+                <h3>From: {info.from_stree}</h3>
+                <h3>To: {info.to_street}</h3>
+              </div>
+            )
+          })
+        }
+      </div>
+    </>
   );
 };
 
