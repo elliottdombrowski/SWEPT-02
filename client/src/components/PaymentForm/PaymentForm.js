9f4ac45acc099074acca32ccab736e51c457e6b3
elliott dombrowski <elliottroyal@me.com>
2022-01-13 14:07:42 -0600

    added stripe docs styling

--- client/src/components/PaymentForm/PaymentForm.js
+++ client/src/components/PaymentForm/PaymentForm.js
@@ -2,6 +2,26 @@ import React, { useState } from 'react';
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import axios from 'axios';
 
+const CARD_OPTIONS = {
+  iconStyle: "solid",
+  style: {
+    base: {
+      iconColor: "#c4f0ff",
+      color: "#fff",
+      fontWeight: 500,
+      fontFamily: "Roboto, Open Sans, Segoe UI, sans-serif",
+      fontSize: "16px",
+      fontSmoothing: "antialiased",
+      ":-webkit-autofill": { color: "#fce883" },
+      "::placeholder": { color: "#87bbfd" }
+    },
+    invalid: {
+      iconColor: "#ffc7ee",
+      color: "#ffc7ee"
+    }
+  }
+};
+
 const PaymentForm = () => {
   const [success, setSuccess] = useState(false);
   const stripe = useStripe();
@@ -45,7 +65,11 @@ const PaymentForm = () => {
           </fieldset>
           <button>Pay</button>
         </form>
-      ) : ()}
+      ) : (
+        <div>
+          <h2>you just bought your devs a coffee.</h2>
+        </div>
+      )}
     </>
   );
 };
