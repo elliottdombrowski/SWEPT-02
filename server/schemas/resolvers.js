490dccb386fff2cfaeff62b9bc45b822a358c832
elliott dombrowski <elliottroyal@me.com>
2022-01-14 15:10:31 -0600

    starter resolver filtering working

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,6 +3,12 @@ const axios = require('axios');
 const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
 
+const lookupWard = (zip) => {
+
+  // Hit city zip lookup
+
+  return 14
+}
 
 
 const resolvers = {
@@ -14,16 +20,18 @@ const resolvers = {
         if (!args.wardNumber) {
           return null;
         }
+        if (args.wardNumber.length == 5) {
+          args.wardNumber = lookupWard(args.wardNumber);
+        }
         const sweeperData = {
           method: 'GET',
-          url: 'https://data.cityofchicago.org/resource/wvjp-8m67.json',
+          url: `https://data.cityofchicago.org/resource/wvjp-8m67.json?ward=${args.wardNumber}`,
           data: {
             '$limit': 5,
             '$$app_token': process.env.SWEEPER
           }
         }
         const sweeperResponse = await axios.request(sweeperData)
-        // console.log(sweeperResponse.data);
         return sweeperResponse.data;
       } catch (error) {
         console.log(error);
