7f0c69860eb22b4412c7e7d9162ac90f5bd55e81
Sadie Sial <sadiesial@gmail.com>
2022-01-14 22:43:48 -0600

    snow to model index.js

--- server/models/index.js
+++ server/models/index.js
@@ -2,9 +2,10 @@
 const User = require('./User');
 const Profile = require('./Profile');
 const Sweeper = require('./Sweeper')
+const Snow = require('./Snow')
 
 
 
 
 // export all schemas
-module.exports = { User, Profile, Sweeper };
+module.exports = { User, Profile, Sweeper, Snow };
