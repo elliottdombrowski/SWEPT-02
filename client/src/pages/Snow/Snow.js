87cc951152cd7aaaa6d4a960e3784976b2d1af47
elliott dombrowski <elliottroyal@me.com>
2022-01-15 14:47:59 -0600

    porting snow form component to street wrapper page

--- client/src/pages/Snow/Snow.js
+++ client/src/pages/Snow/Snow.js
@@ -1,23 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import FindStreetForm from '../../components/FindStreetForm/FindStreetForm';
+import React, { useEffect, useState, useRef } from 'react';
 import Auth from '../../utils/auth';
+import { GET_SNOW } from '../../utils/queries';
+import { useQuery } from '@apollo/client';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import './query.css';
 
 const Snow = () => {
+  const snowNumber = useRef('');
+  const [snow, setSnow] = useState('');
+  const { loading, data } = useQuery(GET_SNOW, {
+    variables: { snowNumber: snow }
+  });
+  const snowInfo = data?.getSnow || [];
+
+  const snowNumberSubmit = async (event) => {
+    event.preventDefault();
+    setSnow(snowNumber.current.value);
+    return true;
+  };
   const saveBtn = Auth.loggedIn ? 'SAVE' : 'LOG IN TO SAVE YOUR RESULTS';
 
   return (
-    <div className='snow-wrapper'>
-      <div className='snow-form-wrapper'>
+    <div className='sweeper-wrapper'>
+      <div className='sweeper-form-wrapper'>
+        <div className='zip-form-wrapper'>
+          <form
+            onSubmit={(event) => snowNumberSubmit(event)}
+            className='zipform-wrapper'
+          >
+            <input
+              // value={}
+              // onChange={}
+              ref={snowNumber}
+              name='snowNumber'
+              placeholder="Enter your Street Name!"
+              className='zipform-input'
+            />
 
-        <FindStreetForm />
-
-      </div>
-
-      <div className='snow-data-output-wrapper'>
-        <button className='login-btn save-btn'>{saveBtn}</button>
+            <button
+              type='submit'
+              className='zipform-input zipform-btn'
+            >
+              Find your schedule!
+            </button>
+          </form>
+        </div>
+        {loading ? (
+          <div>loading...</div>
+        ) : (
+          <div className='sweeper-data-output-wrapper'>
+            {
+              snowInfo.map((info, index) => {
+                return (
+                  <div className='sweeper-data-output' key={index}>
+                    <h2>On: {info.on_street}</h2>
+                    <h3>From: {info.from_stree}</h3>
+                    <h3>To: {info.to_street}</h3>
+                    <button className='login-btn save-btn'>{saveBtn}</button>
+                  </div>
+                )
+              })
+            }
+          </div>
+        )}
       </div>
     </div>
   );
