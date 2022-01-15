f796172c939460aea3ff2eafaf3336b54882f0f6
Sadie Sial <sadiesial@gmail.com>
2022-01-14 22:36:42 -0600

    basic sweeper data schema

--- server/models/Sweeper.js
+++ server/models/Sweeper.js
@@ -2,9 +2,26 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 // sweeperSchema
-
+const sweeperSweeper = new Schema({
+    ward: {
+        type: String,
+        required: true
+    },
+    section: {
+        type: String,
+        required: true
+    },
+    month_name: {
+        type: String,
+        required: true
+    },
+    dates: {
+        type: String,
+        required: true
+    }
+});
 
 
 const Sweeper = mongoose.model('Sweeper', userSchema);
 
-module.exports = User;
+module.exports = Sweeper;
