7ccf41c19fbd7782c8c790efe83718d187fe5894
Sadie Sial <sadiesial@gmail.com>
2022-01-14 22:37:00 -0600

    imported to model index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,10 @@
 // import schemas
 const User = require('./User');
 const Profile = require('./Profile');
+const Sweeper = require('./Sweeper')
+
 
 
 
 // export all schemas
-module.exports = { User, Profile };
+module.exports = { User, Profile, Sweeper };
