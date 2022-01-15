32ae3d7799a6e2634cd34e331c631da151b16f4d
elliott dombrowski <elliottroyal@me.com>
2022-01-14 21:47:50 -0600

    moved homepage queries to app.css

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import SignUpButton from '../../components/SignUpButton/SignUpButton';
-import './query.css';
 
 const Homepage = () => {
   return (
