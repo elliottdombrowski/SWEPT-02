3ef756762ab047fc5b251cf552af11d5d054af14
Sadie Sial <sadiesial@gmail.com>
2022-01-16 17:26:30 -0600

    added with proper fields for mongoose

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -26,6 +26,6 @@ export const ADD_USER = gql`
 
 export const SAVE_SWEEPER = gql`
 mutation saveSweeper($ward: String!, $section: String!, $month_name: String!, $dates: String!, $zipcode: String!, $user: String!) {
-  
+  saveSweeper(ward: $ward, section: $section, month_name: $month_name, dates: $dates, zipcode: $zipcode, user: $user)
 }
 `;
