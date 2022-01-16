514402155e422247117a21aa194fd7f58efe7308
Sadie Sial <sadiesial@gmail.com>
2022-01-16 11:35:43 -0600

    mutation for saving

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -43,9 +43,9 @@ const typeDefs = gql`
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addWard()
-    addZip()
-    addSnow()
+    addWard(: ): Ward
+    addZip(: ): Zip
+    addSnow(: ): 
   }
   `;
 
