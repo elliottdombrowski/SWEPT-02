c6f18dc79885d8469be6796ec412eb6a26ceab2e
Sadie Sial <sadiesial@gmail.com>
2022-01-16 10:31:06 -0600

    snow model set

--- server/models/Snow.js
+++ server/models/Snow.js
@@ -1,23 +1,23 @@
-// const mongoose = require('mongoose');
-// const { Schema } = mongoose;
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
-// // snowSchema (best to keep seperate probs)
-// const snowSchema = new Schema({
-//     on_street: {
-//         type: String,
-//         required: true
-//     },
-//     // 'stree' ON PURPOSE due to API error
-//     from_stree: {
-//         type: String,
-//         required: true
-//     },
-//     to_street: {
-//         type: String,
-//         required: true
-//     }
-// })
+// snowSchema (best to keep seperate probs)
+const snowSchema = new Schema({
+    on_street: {
+        type: String,
+        required: true
+    },
+    // 'stree' ON PURPOSE due to API error
+    from_stree: {
+        type: String,
+        required: true
+    },
+    to_street: {
+        type: String,
+        required: true
+    }
+})
 
-// const Snow = mongoose.model('Snow', snowSchema);
+const Snow = mongoose.model('Snow', snowSchema);
 
-// module.exports = Snow;
+module.exports = Snow;
