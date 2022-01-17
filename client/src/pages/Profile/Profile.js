7ec3ac26e790b7c86e528251d1bce99f6514f080
elliott dombrowski <elliottroyal@me.com>
2022-01-17 14:15:05 -0600

    added resolvers

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Redirect } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_SINGLE_USER } from '../../utils/queries';
 import Auth from '../../utils/auth';
