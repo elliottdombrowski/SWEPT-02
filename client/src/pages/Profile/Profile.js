c7a57687e524ed35dcec4deaeaa726a3e05bd8cf
Sadie Sial <sadiesial@gmail.com>
2022-01-18 17:51:48 -0600

    import and copy from user for test

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { QUERY_ME, QUERY_USER_SWEEPERS } from '../../utils/queries';
+import { QUERY_ME, QUERY_USER_SWEEPERS, QUERY_USER_SNOW } from '../../utils/queries';
 import Auth from '../../utils/auth';
 import LoginSignup from '../LoginSignup/LoginSignup';
 
 const Profile = () => {
   const { username: userParam } = useParams();
+
+  const { loading, data } = useQuery(QUERY_ME, {
+    variables: { username: userParam },
+  });
+
+  const { loading, data } = useQuery(QUERY_ME, {
+    variables: { username: userParam },
+  });
 
   const { loading, data } = useQuery(QUERY_ME, {
     variables: { username: userParam },
