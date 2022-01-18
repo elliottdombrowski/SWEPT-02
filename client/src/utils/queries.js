12f3173b81cd1b53e6166518d72da23dbb7a4730
Sadie Sial <sadiesial@gmail.com>
2022-01-18 17:56:31 -0600

    testing with quereies

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -55,25 +55,9 @@ export const QUERY_ME = gql`
 `;
 
 export const QUERY_USER_SWEEPERS = gql`
-  query getUserSweepers($userId: String!) {
-    getUserSweepers(userId: $userId) {
-      ward
-      section
-      month_name
-      dates
-      user
-    }
-  }
-`;
-
-export const QUERY_USER_SNOW = gql`
-  query getUserSnow($userId: String!) {
-    getUserSnow(userId: $userId) {
-      on_street
-      from_stree
-      to_street
-      restrict_t
-      user
+  query getUserSweepers($user: String!) {
+    getUserSweepers(user: $user) {
+     
     }
   }
 `;
