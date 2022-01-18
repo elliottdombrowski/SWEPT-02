f058f57e4a23a1b2be62fa394abad5d4d5fd0081
elliott dombrowski <elliottroyal@me.com>
2022-01-17 19:21:04 -0600

    added styling and back to home link to 404

--- client/src/pages/NotFound/NotFound.js
+++ client/src/pages/NotFound/NotFound.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
+
+let arrow = <FontAwesomeIcon icon={faArrowLeft} />
 
 const NotFound = () => {
   return (
     <div className='notfound-wrapper'>
       <div className='notfound-content'>
         <img className='notfound-img' src={require('../../assets/ogsweeper2.png')}></img>
+        <h1 className='notfound-header'>
+          Whoops! The page you're looking for doesn't exist.
+        </h1>
+        <button className='login-btn notfound-btn'>
+          <Link to='/' className='notfound-link'>
+            {arrow} back to homepage
+          </Link>
+        </button>
       </div>
     </div>
   );
