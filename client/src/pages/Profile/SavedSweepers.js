a84612b3ef5809e00e680dc21c075df63191794d
Sadie Sial <sadiesial@gmail.com>
2022-01-18 18:03:48 -0600

    savedsweeper fx

--- client/src/pages/Profile/SavedSweepers.js
+++ client/src/pages/Profile/SavedSweepers.js
@@ -1,6 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { QUERY_ME, QUERY_USER_SWEEPERS, QUERY_USER_SNOW } from '../../utils/queries';
+import { QUERY_ME, QUERY_USER_SWEEPERS} from '../../utils/queries';
 import Auth from '../../utils/auth';
 import LoginSignup from '../LoginSignup/LoginSignup';
+
+const SavedSweepers = () => {
+
+}
+
+export default SavedSweepers;
