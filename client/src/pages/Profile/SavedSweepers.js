bb92f297c06005c66ea98051f9a90fdf9e51e781
Sadie Sial <sadiesial@gmail.com>
2022-01-19 00:44:46 -0600

    delete mutations with deleteSweeper by id

--- client/src/pages/Profile/SavedSweepers.js
+++ client/src/pages/Profile/SavedSweepers.js
@@ -5,13 +5,26 @@ import { QUERY_USER_SWEEPERS } from '../../utils/queries';
 
 const SavedSweepers = () => {
     const [userId, setUserId] = useState(localStorage.getItem('uuid'));
+    const [updatedSweepers, setUpdatedSweepers] = useState();
 
     const { loading, data } = useQuery(QUERY_USER_SWEEPERS, {
         variables: { user: userId },
     });
 
     const userSweepers = data?.getUserSweepers || [];
-    console.log(userSweepers)
+    // setUpdatedSweepers(userSweepers);
+    const [deleteSweeper, { data: deletedSweeper, loading: deletedSweeperLoading}] = useMutation(MUTATION_DELETE_SWEEPER)
+
+    const handleDeleteSweeper= (id) => {
+        deleteSnow({ variables: { id } })
+    }
+    // setUpdatedSnow(userSnow);
+    useEffect(() => {
+        if (deletedSweeperData) {
+            alert("sweeper deleted")
+        }
+    }, [deletedSweeperData])
+
 
     return (
         <>
@@ -27,7 +40,7 @@ const SavedSweepers = () => {
                                     <h2>Ward: {singleSweeper.ward}</h2>
                                     <h3>{singleSweeper.month_name}</h3>
                                     <h4>on dates: {singleSweeper.dates}</h4>
-                                    <button onClick={() => alert(singleSweeper._id)}>Delete</button>
+                                    <button onClick={() => handleDeleteSweeper(singleSweeper._id)}>Delete</button>
                                 </div>
                             )
                         })
