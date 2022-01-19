abc1c4443f9c73cdc0b6a078b8cb7a61219d5301
Sadie Sial <sadiesial@gmail.com>
2022-01-19 00:39:32 -0600

    added comments

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -54,6 +54,7 @@ export const QUERY_ME = gql`
   }
 `;
 
+// save to db
 export const QUERY_USER_SWEEPERS = gql`
   query getUserSweepers($user: String!) {
     getUserSweepers(user: $user) {
@@ -67,6 +68,7 @@ export const QUERY_USER_SWEEPERS = gql`
   }
 `;
 
+// save to db
 export const QUERY_USER_SNOW = gql`
   query getUserSnow($user: String!) {
     getUserSnow(user: $user) {
@@ -80,12 +82,14 @@ export const QUERY_USER_SNOW = gql`
   }
 `;
 
+// delete from db
 export const MUTATION_DELETE_SWEEPER = gql`
   mutation deleteSweeper($id: String!){
     deleteSweeper(id: $id)
   }
 `
 
+// delete from db
 export const MUTATION_DELETE_SNOW = gql`
   mutation deleteSnow($id: String!){
     deleteSnow(id: $id)
