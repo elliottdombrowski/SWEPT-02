6cd4cb7f4e615fabf0c6c7441fc31c09976671f2
Sadie Sial <sadiesial@gmail.com>
2022-01-18 18:04:09 -0600

    copied in from Profile.js

--- client/src/pages/Profile/SavedSweepers.js
+++ client/src/pages/Profile/SavedSweepers.js
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-import { QUERY_ME, QUERY_USER_SWEEPERS} from '../../utils/queries';
+import { QUERY_ME, QUERY_USER_SWEEPERS } from '../../utils/queries';
 import Auth from '../../utils/auth';
 import LoginSignup from '../LoginSignup/LoginSignup';
 
 const SavedSweepers = () => {
+
+    const { loading_sweeper, sweeperData } = useQuery(QUERY_USER_SWEEPERS, {
+        variables: { user: userId },
+    });
+
 
 }
 
